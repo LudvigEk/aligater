d7148c41cb35c7e91d2fb989ca044662446e9d7a
Ludvig <ludvig.ekdahl@med.lu.se>
2021-03-31 16:25:01 +0200

    agconfig defaults

--- aligater/AGConfig.py
+++ aligater/AGConfig.py
@@ -2,6 +2,8 @@
 #These flags/variables are accessible throughout aligater under namespace agconf
 #example: agconf.minCells, agconf.ag_verbose
 
+#Make sure to review and set up these filters to reasonable values for your flow cytometry setup
+
 #If a population has less cells than this threshold, any population defined with this population as parent will be flagged invalid
 minCells=100000
 
@@ -19,14 +21,16 @@ ag_maxMeasurement = 214000
 #Sets verbosity level, if False only minor information and warnings are printed. Does not affect plotting behaviour.
 ag_verbose=False
 
-#AliGater output dir
-ag_out="/media/ludvig/Project_Storage/BloodVariome/aligater_output"
-
-#AliGater tempdir/scratch space (intermediate files such as downsampled images)
-ag_tmp="/media/ludvig/Project_Storage/aligater_temp"
-
 import os.path
 ag_home = os.path.dirname(os.path.dirname(__file__)) #AliGater Root folder, wherever it was
+
+#AliGater output dir, change if needed
+ag_out=os.path.join(ag.agconf.ag_home, "out")
+
+#AliGater tempdir/scratch space (intermediate files such as downsampled images), change if needed
+ag_tmp=os.path.join(ag.agconf.ag_home, "temp")
+
+
 
 #Check ag dirs are valid
 from aligater.check_exec_mode import check_agconfig #CHECK THAT THIS SANITIZES PATH
