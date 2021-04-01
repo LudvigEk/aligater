88e7c33a96da1a0c6e5b8eddbaf2aee0fccb1a4d
Ludvig <ludvig.ekdahl@med.lu.se>
2021-04-01 12:18:12 +0200

    requirement tweaks

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def extensions():
     sourcefiles = ['aligater/AGCython.pyx']
     ext =Extension("aligater.AGCython", sourcefiles,libraries=["m"],language='c++', include_dirs=[np.get_include(),"./aligater"])
     sourcefiles1 = ['aligater/AGCythonUtils.pyx']
-    ext1 =Extension("aligater.AGCythonUtils", sourcefiles1,libraries=["m"],language='c++', include_dirs=[np.get_include(),"./aligater"])
+    ext1 =Extension("aligater.AGCythonUtils", sourcefiles1,libraries=["m"],language='c', include_dirs=[np.get_include(),"./aligater"])
     return cythonize([ext,ext1])
 
 
